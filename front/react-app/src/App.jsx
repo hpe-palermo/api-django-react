e87a607d59874e01fe1734a49ee005c2bfb76588
Palermo <124798752+hpe-palermo@users.noreply.github.com>
2024-07-19 10:41:32 -0300

    Add files via upload

--- front/react-app/src/App.jsx
+++ front/react-app/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [color, setColor] = useState('');
+  const [inputColor, setInputColor] = useState('');
 
   function getColor() {
     const URL = 'http://127.0.0.1:8000/get-color/';
@@ -13,15 +14,38 @@ function App() {
       .catch(error => console.log("Error fetching color:", error));
   }
 
+  function postColor() {
+    const URL = 'http://127.0.0.1:8000/post-color/';
+    const data = { color: inputColor || 'yellow' };
+
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(data => setColor(data.color))
+      .catch(error => console.log("Error fetching color:", error));
+  }
+
   return (
     <div className="App">
       <input
         type="text"
-        placeholder='This color is:'
-        readOnly
-        value={color}
+        placeholder='Enter color'
+        value={inputColor}
+        onChange={(e) => setInputColor(e.target.value)}
       />
       <button onClick={getColor}>GET</button>
+      <button onClick={postColor}>POST</button>
+      <input
+        type="text"
+        placeholder='This color is:'
+        readOnly
+        value={color || ''}
+      />
     </div>
   );
 }
