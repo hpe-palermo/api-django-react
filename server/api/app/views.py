28811fcda2f2e50600a6f19f8f495cdfb013da02
Palermo <124798752+hpe-palermo@users.noreply.github.com>
2024-07-19 10:36:28 -0300

    Update views.py

--- server/api/app/views.py
+++ server/api/app/views.py
@@ -2,7 +2,6 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-# Supondo que `color` seja uma lista global
 color = ['red']
 
 def get_color(request):
